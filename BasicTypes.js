53b361f54bd7deaba6effb1a8b43e62424964ff8
Bralex-svg <alexadam269@gmail.com>
2023-02-22 12:28:13 -0800

    first commit

---
+++ BasicTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+//defining types
+var username;
+username = "alex";
+var age = 23;
+var isActive;
+isActive = true;
+//any is should not be mostly used unless for special purposes.
+var school;
+school = 44;
---
+++ BasicTypes.ts
@@ -0,0 +1,15 @@
+
+export {}
+
+//defining types
+
+let username: string;
+username = "alex"
+let age: number = 23;
+
+let isActive: boolean;
+isActive = true
+
+//any is should not be mostly used unless for special purposes.
+let school: any
+school = 44
