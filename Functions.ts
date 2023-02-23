7953c6d96652e222480a7b40ddeedd5125ff8dfa
Bralex-svg <alexadam269@gmail.com>
2023-02-23 15:53:50 -0800

    functions

---
+++ Functions.ts
@@ -0,0 +1,12 @@
+const add = (a: number, b: number) =>{
+    return a + b
+}
+
+console.log(add(5, 6));
+
+
+//You can use void if the function will not return any value
+
+function sub(message: any): void {
+
+}
