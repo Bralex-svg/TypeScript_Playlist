01e86a8f40c44a122100c7bf91f1668145189013
Bralex-svg <alexadam269@gmail.com>
2023-02-23 11:50:08 -0800

    added tuples

---
+++ Tuples.ts
@@ -0,0 +1,15 @@
+// Tuple
+//Tuples can contain two values of different types
+
+let tuple: [string, number, boolean] = ["hello", 43, true]
+
+let details: [string, number, boolean];
+details = ["alex", 23, true]
+details[0] // returns "alex"
+details[1] // returns 23
+// you can add new elements to a tuple using the push method
+const employee: [number, string] = [1, "adam"]
+employee.push(2, "alex")
+console.log(employee);
+
+
