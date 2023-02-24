779bc85aa8ccb8ac719b796bde9bc66192218d4d
Bralex-svg <alexadam269@gmail.com>
2023-02-24 12:04:05 -0800

    functions added

--- Functions.ts
+++ Functions.ts
@@ -1,5 +1,5 @@
 const add = (a: number, b: number) =>{
-    return a + b
+    return a + b;
 }
 
 console.log(add(5, 6));
@@ -8,5 +8,9 @@ console.log(add(5, 6));
 //You can use void if the function will not return any value
 
 function sub(message: any): void {
+console.log(message);
+}
 
+let subtract = (a: number, b: number) : number =>{
+    return a -b
 }
